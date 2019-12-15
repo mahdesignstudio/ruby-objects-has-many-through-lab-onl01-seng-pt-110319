6398ca433071a720ad311dfb32dda26a45000a75
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-15 13:32:43 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,17 +1,19 @@
 class Appointment 
-  
-attr_accessor :date, :patient, :doctor 
 
-@@all = [ ]
+    attr_accessor :date, :patient, :doctor
 
-  def initialize(date, patient, doctor)
-    @date = date
-    @patient = patient 
-    @doctor = doctor 
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-end 
+    @@all = []
+
+    def initialize(date, patient, doctor)
+        @date = date
+        @patient = patient 
+        @doctor = doctor
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    
+end
