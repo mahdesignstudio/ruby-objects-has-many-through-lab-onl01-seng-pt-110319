7491e2899fde3a285eb5132e039e8a91da74e76c
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-15 13:24:39 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,34 +1,27 @@
 class Patient 
   
-attr_reader :name 
+  attr_reader :name 
 
-@all = [ ]
+  @all = [ ]
 
-def initialize(name)
-  @name = name 
-  @@all << self 
-end 
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
 
-def self.all
-  @@all
-end 
+  def self.all
+    @@all
+  end 
 
-def new_appointment(doctor, date)
-  Appointment.new(date, self, doctor)
-end 
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end 
 
-def appointments 
-  Appointment.all.select {|appointment|appointment.patient == self} 
-end 
+  def appointments 
+    Appointment.all.select {|appointment|appointment.patient == self} 
+  end 
 
-def doctors 
-  appointments.map {|appointment|appointment.doctor } 
-end 
-  
-  
-  
-  
-  
-  
-  
+  def doctors 
+    appointments.map {|appointment|appointment.doctor } 
+  end 
 end 
