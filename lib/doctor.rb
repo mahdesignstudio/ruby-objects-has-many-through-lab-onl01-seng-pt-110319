4339a0cbf74195f3408677f94ef5fbc221fff309
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-15 13:11:22 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,23 @@
+class Doctor
+
+attr_reader :name
+
+@@all = [ ]
+
+def initialize(name)
+  @name = name 
+  @@all << self 
+end 
+
+def new_appointment 
+
+
+def self.all 
+  @@all
+end 
+
+
+
+
+
+end 
